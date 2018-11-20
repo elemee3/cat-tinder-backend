22c5daa311054d4bcafbdf61aaa04ee23e186f07
learn <dkellam9@gmail.com>
2018-11-19 17:16:19 -0800

    form not quite submitting correctly

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,34 @@
 class CatsController < ApplicationController
+  before_action :set_cat, only: [:show, :update, :destroy]
+
   def index
     cats = Cat.all
     render json: cats
+  end
+
+  def show
+    render json: @cat
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+  end
+
+  def update
+    @cat.update_attributes(cat_params)
+  end
+
+  def destroy
+    @cat.delete
+  end
+
+  private
+
+  def set_cat
+    @cat = Cat.find(params[:id])
+  end
+
+  def cat_params
+    params.require(:cat).permit(:name, :age, :enjoys)
   end
 end
