de379774fdb728b1e509ae9f8c09f27541f8a24a
learn <dkellam9@gmail.com>
2018-11-21 12:32:06 -0800

    deployment v2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://protected-citadel-24964.herokuapp.com/'
 
     resource '*',
       headers: :any,
